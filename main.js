618236ff3b91a6fa40f7d5ad93b81237a7387f4b
CIEHSRCC <134692517+CIEHSRCC@users.noreply.github.com>
2023-05-28 22:07:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-﻿
+
 
 //Function for Hamburger menu
 
@@ -70,8 +70,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML = days + " Days " + hours + " Hours "
+  + minutes + " Minutes <br>" + seconds + " Seconds ";
     
   // If the count down is over, write some text 
   if (distance < 0) {
